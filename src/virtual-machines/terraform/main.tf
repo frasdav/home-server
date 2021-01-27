87b6d63831d8e1a2adba1145f767093fdef1d1bb
Fraser Davidson <fraser@fdavidson.com>
2021-01-27 17:08:40 +0000

    WIP

--- src/virtual-machines/terraform/main.tf
+++ src/virtual-machines/terraform/main.tf
@@ -26,8 +26,8 @@ resource "vsphere_virtual_machine" "kube_master" {
 
   guest_id = "ubuntu64Guest"
 
-  num_cpus         = 2
-  memory           = 4096
+  num_cpus         = 4
+  memory           = 8192
   enable_disk_uuid = true
 
   network_interface {
@@ -78,8 +78,8 @@ resource "vsphere_virtual_machine" "kube_worker" {
 
   guest_id = "ubuntu64Guest"
 
-  num_cpus         = 2
-  memory           = 8192
+  num_cpus         = 4
+  memory           = 16384
   enable_disk_uuid = true
 
   network_interface {
